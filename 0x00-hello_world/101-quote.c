0e99155538a6849670e9c48202a7c2ca9b8efdb4
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-10-13 09:39:41 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main-it prints to standard error
+ *
+ *Return: 1 if successful
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\"- Dora korpar, 2015-10-19\n",61);
+	return (0);
+}
