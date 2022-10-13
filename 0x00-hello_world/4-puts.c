f06270169b7ecfc8e09ca4d5bca7404b487f93e1
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-10-13 06:30:29 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- It runs the puts function which prints its codeblock
+ *
+ * returns 0 if successful
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}
