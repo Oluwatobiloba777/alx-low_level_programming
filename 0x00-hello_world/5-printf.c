a7f3acafb00c53164d0c3b8397af081a79aaaf0f
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-10-13 08:43:23 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- it runs the printf functions which prints it code block
+ *
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
