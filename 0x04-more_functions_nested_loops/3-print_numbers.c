7263595f523f0120542dbd066acd31d4ff98dd9b
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-10-20 17:13:02 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers - it prints numbers from 0-9
+ *
+ *Return: 0 if successfull
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar("\n");
+}
