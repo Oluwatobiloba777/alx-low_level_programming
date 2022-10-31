396218e82e2b46d3c75c37cf54106b9f0ff7757a
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-10-31 16:02:05 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,15 +9,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int b, c;
+	int a, c;
 
 	for (b = 0; b < 8; b++)
 	{
 		for (c = 0; c < 8; c++)
 		{
-			_putchar (*(*(b + a) + c));
+			_putchar(*(*(b + a) + c));
 		}
+	_putchar('\n');
 	}
-
-	_putchar ('\n');
 }
