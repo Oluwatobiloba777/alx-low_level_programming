a2fa8c46348c0f75230a1e69f3d25bb1dbb2286d
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-11-03 00:47:46 -0700

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion- get length of a string
+ *@s: string
+ *
+ *Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s != 0)
+	{
+		a = _strlen_recursion(s + 1);
+		return (++a);
+	}
+
+	return (a);
+}
+
+/**
+ *check_palindrome- check if a string is a palindrom
+ *@s: string
+ *@a:position
+ *
+ *Return: int
+ */
+
+int check_palindrome(char *s, int a)
+{
+	int a = _strlen_recursion(s) - 1;
+
+	if (*s == s[a])
+	{
+		s++;
+		a++;
+	}
+	else
+	{
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ *is_palindrome-returns if a string is a palindrome
+ *@s: string
+ *
+ *Return: int
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '0')
+		return (1);
+	return (check_palindrome(s));
+}
