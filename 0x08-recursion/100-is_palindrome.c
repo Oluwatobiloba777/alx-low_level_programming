9d32a427fdb93089ab85caa21aa6fe334759c2c1
Oluwatobiloba777 <iamtobihunkuten@gmail.com>
2022-11-03 01:10:31 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,58 +1,46 @@
 #include "main.h"
 
 /**
- *_strlen_recursion- get length of a string
- *@s: string
- *
- *Return: string length
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
-
 int _strlen_recursion(char *s)
 {
-	int a = 0;
-
-	if (*s != 0)
-	{
-		a = _strlen_recursion(s + 1);
-		return (++a);
-	}
-
-	return (a);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 
 /**
- *check_palindrome- check if a string is a palindrom
- *@s: string
- *
- *Return: int
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
  */
 
-int check_palindrome(char *s)
+int comparator(char *s, int n1, int n2)
 {
-	int b = _strlen_recursion(s) - 1;
-
-	if (*s == s[b])
+	if (*(s + n1) == *(s + n2))
 	{
-		s++;
-		b--;
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
-	else
-	{
-		return (0);
-	}
-	return (1);
+	return (0);
 }
 
 /**
- *is_palindrome-returns if a string is a palindrome
- *@s: string
- *
- *Return: int
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
 
 int is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	return (check_palindrome(s - 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
